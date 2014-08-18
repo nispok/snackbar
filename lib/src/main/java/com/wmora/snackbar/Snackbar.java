ab39229a312e609d60fcbf57fab5c3cf60c97f6b
William Mora <wilmor24@gmail.com>
2014-08-18 19:50:01 -0300

    first commit

---
+++ lib/src/main/java/com/wmora/snackbar/Snackbar.java
@@ -0,0 +1,270 @@
+package com.wmora.snackbar;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.wmora.snackbar.listeners.SwipeDismissTouchListener;
+
+public class Snackbar extends RelativeLayout {
+
+    public enum SnackbarType {
+        SINGLE_LINE(48, 1), MULTI_LINE(80, 2);
+
+        private int height;
+        private int maxLines;
+
+        SnackbarType(int height, int maxLines) {
+            this.height = height;
+            this.maxLines = maxLines;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public int getMaxLines() {
+            return maxLines;
+        }
+    }
+
+    public enum SnackbarDuration {
+        LENGTH_SHORT(2000), LENGTH_LONG(3500);
+
+        private long duration;
+
+        SnackbarDuration(long duration) {
+            this.duration = duration;
+        }
+
+        public long getDuration() {
+            return duration;
+        }
+    }
+
+    private SnackbarType mType = SnackbarType.SINGLE_LINE;
+    private SnackbarDuration mDuration = SnackbarDuration.LENGTH_LONG;
+    private CharSequence mText;
+    private int mColor = 0xff323232;
+    private int mTextColor = Color.WHITE;
+    private CharSequence mActionLabel;
+    private int mActionColor = Color.GREEN;
+    private OnClickListener mActionListener;
+    private boolean mAnimated = true;
+
+    private Snackbar(Context context) {
+        super(context);
+    }
+
+    public static Snackbar with(Context context) {
+        return new Snackbar(context);
+    }
+
+    /**
+     * Sets the type of {@link com.wmora.snackbar.Snackbar} to be displayed.
+     * @param type the {@link com.wmora.snackbar.Snackbar.SnackbarType} of this instance
+     * @return
+     */
+    public Snackbar type(SnackbarType type) {
+        mType = type;
+        return this;
+    }
+
+    /**
+     * Sets the text to be displayed in this {@link com.wmora.snackbar.Snackbar}
+     * @param text
+     * @return
+     */
+    public Snackbar text(CharSequence text) {
+        mText = text;
+        return this;
+    }
+
+    /**
+     * Sets the background color of this {@link com.wmora.snackbar.Snackbar}
+     * @param color
+     * @return
+     */
+    public Snackbar color(int color) {
+        mColor = color;
+        return this;
+    }
+
+    /**
+     * Sets the text color of this {@link com.wmora.snackbar.Snackbar}
+     * @param textColor
+     * @return
+     */
+    public Snackbar textColor(int textColor) {
+        mTextColor = textColor;
+        return this;
+    }
+
+    /**
+     * Sets the action label to be displayed, if any. Note that if this is not set, the action
+     * button will not be displayed
+     * @param actionButtonLabel
+     * @return
+     */
+    public Snackbar actionLabel(CharSequence actionButtonLabel) {
+        mActionLabel = actionButtonLabel;
+        return this;
+    }
+
+    /**
+     * Sets the color of the action button label. Note that you must set a button label with
+     * {@link com.wmora.snackbar.Snackbar#actionLabel(CharSequence)} for this button to be displayed
+     * @param actionColor
+     * @return
+     */
+    public Snackbar actionColor(int actionColor) {
+        mActionColor = actionColor;
+        return this;
+    }
+
+    /**
+     * Sets the listener to be called when the {@link com.wmora.snackbar.Snackbar} action is
+     * selected. Note that you must set a button label with
+     * {@link com.wmora.snackbar.Snackbar#actionLabel(CharSequence)} for this button to be displayed
+     * @param listener
+     * @return
+     */
+    public Snackbar actionListener(OnClickListener listener) {
+        mActionListener = listener;
+        return this;
+    }
+
+    /**
+     * Sets on/off animation for this {@link com.wmora.snackbar.Snackbar}
+     * @param withAnimation
+     * @return
+     */
+    public Snackbar animation(boolean withAnimation) {
+        mAnimated = withAnimation;
+        return this;
+    }
+
+    /**
+     * Sets the duration of this {@link com.wmora.snackbar.Snackbar}. See
+     * {@link com.wmora.snackbar.Snackbar.SnackbarDuration} for available options
+     * @param duration
+     * @return
+     */
+    public Snackbar duration(SnackbarDuration duration) {
+        mDuration = duration;
+        return this;
+    }
+
+    private void init(Activity parent) {
+        RelativeLayout layout = (RelativeLayout) LayoutInflater.from(parent)
+                .inflate(R.layout.snackbar, this, true);
+
+        layout.setBackgroundColor(mColor);
+
+        float scale = parent.getResources().getDisplayMetrics().density;
+        int height = (int) (mType.getHeight() * scale + 0.5f);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, height);
+
+        layout.setLayoutParams(params);
+
+        TextView snackbarText = (TextView) layout.findViewById(R.id.snackbar_text);
+        snackbarText.setText(mText);
+        snackbarText.setTextColor(mTextColor);
+        snackbarText.setMaxLines(mType.getMaxLines());
+
+        TextView snackbarAction = (TextView) layout.findViewById(R.id.snackbar_action);
+        if (!TextUtils.isEmpty(mActionLabel)) {
+            snackbarAction.setText(mActionLabel);
+            snackbarAction.setTextColor(mActionColor);
+            snackbarAction.setOnClickListener(mActionListener);
+        } else {
+            snackbarAction.setVisibility(GONE);
+        }
+
+        setClickable(true);
+
+        setOnTouchListener(new SwipeDismissTouchListener(this, null,
+                new SwipeDismissTouchListener.DismissCallbacks() {
+                    @Override
+                    public boolean canDismiss(Object token) {
+                        return true;
+                    }
+
+                    @Override
+                    public void onDismiss(View view, Object token) {
+                        if (view != null) {
+                            dismiss();
+                        }
+                    }
+                }));
+    }
+
+    /**
+     * Displays the {@link com.wmora.snackbar.Snackbar} at the bottom of the
+     * {@link android.app.Activity} provided.
+     * @param targetActivity
+     */
+    public void show(Activity targetActivity) {
+        init(targetActivity);
+        ViewGroup root = (ViewGroup) targetActivity.findViewById(android.R.id.content);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.BOTTOM;
+        root.addView(this, params);
+
+        if (mAnimated) {
+            startSnackbarAnimation();
+        } else {
+            postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    dismiss();
+                }
+            }, mDuration.getDuration());
+        }
+    }
+
+    private void startSnackbarAnimation() {
+        Animation slideIn = AnimationUtils.loadAnimation(getContext(), R.anim.slide_in_from_bottom);
+        Animation fadeOut = AnimationUtils.loadAnimation(getContext(), R.anim.fade_out);
+        fadeOut.setStartOffset(mDuration.getDuration());
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                dismiss();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        AnimationSet animationSet = new AnimationSet(false);
+        animationSet.addAnimation(slideIn);
+        animationSet.addAnimation(fadeOut);
+        startAnimation(animationSet);
+    }
+
+    private void dismiss() {
+        clearAnimation();
+        ((ViewGroup) getParent()).removeView(this);
+    }
+}
