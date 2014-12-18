9bdff859f70535bf4a3ed16b1eba6df070cd675c
William Mora <wilmor24@gmail.com>
2014-12-18 00:12:16 -0300

    SnackbarManager included in lib. Closes #35

---
+++ lib/src/main/java/com/nispok/snackbar/SnackbarManager.java
@@ -0,0 +1,51 @@
+package com.nispok.snackbar;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+/**
+ * A handler for multiple {@link Snackbar}s
+ */
+public class SnackbarManager {
+
+    private static final String TAG = SnackbarManager.class.getSimpleName();
+
+    private static Snackbar currentSnackbar;
+
+    private SnackbarManager() {
+    }
+
+    /**
+     * Displays a {@link com.nispok.snackbar.Snackbar} in the current {@link Activity}, dismissing
+     * the current Snackbar being displayed, if any. Note that the Activity will be obtained from
+     * the Snackbar's {@link android.content.Context}. If the Snackbar was created with
+     * {@link android.app.Activity#getApplicationContext()} then you must explicitly pass the target
+     * Activity using {@link #show(Snackbar, android.app.Activity)}
+     *
+     * @param snackbar instance of {@link com.nispok.snackbar.Snackbar} to display
+     */
+    public static void show(@NonNull Snackbar snackbar) {
+        try {
+            show(snackbar, (Activity) snackbar.getContext());
+        } catch (ClassCastException e) {
+            Log.e(TAG, "Couldn't get Activity from the Snackbar's Context. Try calling " +
+                    "#show(Snackbar, Activity) instead", e);
+        }
+    }
+
+    /**
+     * Displays a {@link com.nispok.snackbar.Snackbar} in the current {@link Activity}, dismissing
+     * the current Snackbar being displayed, if any
+     *
+     * @param snackbar instance of {@link com.nispok.snackbar.Snackbar} to display
+     * @param activity target {@link Activity} to display the Snackbar
+     */
+    public static void show(@NonNull Snackbar snackbar, @NonNull Activity activity) {
+        if (currentSnackbar != null) {
+            currentSnackbar.dismiss();
+        }
+        currentSnackbar = snackbar;
+        currentSnackbar.show(activity);
+    }
+}
