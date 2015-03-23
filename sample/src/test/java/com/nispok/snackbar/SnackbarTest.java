764f1c5e43a124adef2c512233cb9f65138bd643
William Mora <wilmor24@gmail.com>
2015-03-23 19:22:42 -0300

    tests excecuted from sample app

--- sample/src/test/java/com/nispok/snackbar/SnackbarTest.java
+++ sample/src/test/java/com/nispok/snackbar/SnackbarTest.java
@@ -1,6 +1,7 @@
 package com.nispok.snackbar;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 
 import com.nispok.snackbar.enums.SnackbarType;
